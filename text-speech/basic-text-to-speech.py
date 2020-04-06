2cc0745b02a46a3921c30c13203a531c30c2f25a
tafoca <devtafo@gmail.com>
2020-04-06 11:58:29 +0100

    fix bug played audio

--- text-speech/basic-text-to-speech.py
+++ text-speech/basic-text-to-speech.py
@@ -1,7 +1,11 @@
 from gtts import gTTS
 import os
+#[comment] : pip install ffmpeg-python
+from pydub import AudioSegment
+from pydub.playback import play
+
 #case google api gtts google text to speech
-myText = ""
+myText = "Text to speech Conversion using python and gtts."
 language = 'en'
 def main(): 
     
@@ -11,7 +15,9 @@ def main():
                   )
     output.save("ouput.mp3")#save audio in this name
     
-    os.system("start ouput.mp3") #start audio by default program reading
     
+    #os.system("start ouput.mp3") #start audio by default program reading
+    sound = AudioSegment.from_mp3('ouput.mp3')
+    play(sound)
 if __name__ == "__main__":
     main()
